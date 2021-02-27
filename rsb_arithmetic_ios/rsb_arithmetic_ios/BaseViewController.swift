fb29b7c5c16967a4126473526e57d2c8d7867284
Roman Brazhnikov <cold.steel.br@gmail.com>
2021-02-27 14:17:18 +0700

    Main VC removed, ChooseOperationVC added

--- rsb_arithmetic_ios/rsb_arithmetic_ios/BaseViewController.swift
+++ rsb_arithmetic_ios/rsb_arithmetic_ios/BaseViewController.swift
@@ -9,47 +9,4 @@ import UIKit
 
 class BaseViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        registerKeyboardNotifications()
-        
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        self.view.addGestureRecognizer(tap)
-    }
-    
-    private func registerKeyboardNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-//
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)), name: UIResponder.keyboardDidChangeFrameNotification, object: nil)
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-    }
-    @objc
-    private func keyboardWillChange(notification: Notification) {
-        guard let kFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue,
-              let duration = (notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue
-        else { return }
-        
-        print("\(notification.name) frame: \(kFrame), Y = \(kFrame.minY)")
-        
-        onKeyboardHeightChanged(height: view.frame.height - kFrame.minY, duration: duration)
-    }
-    
-    @objc func dismissKeyboard() {
-        self.view.endEditing(true)
-    }
-    
-    open func onKeyboardHeightChanged(height: CGFloat, duration: Double) {
-        
-    }
-    
-    
 }
---
+++ rsb_arithmetic_ios/rsb_arithmetic_ios/ChooseOperationViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ChooseOptionViewController.swift
+//  rsb_arithmetic_ios
+//
+//  Created by Roman Brazhnikov on 27.02.2021.
+//
+
+class ChooseOperationViewController: BaseViewController {
+    
+}
